d994f4518c65578bde1b58d7c7918bdac695906d
kenancal <kenancal@gmail.com>
2026-06-19 00:03:26 +0300

    Giris OS kaldirildi, baslik 1/3

--- atolye_os/src/Login.jsx
+++ atolye_os/src/Login.jsx
@@ -39,7 +39,7 @@ function Login() {
           <div className="brandMark">A</div>
           <div>
             <p className="eyebrow">Kişisel girişim merkezi</p>
-            <h1>Atölye OS</h1>
+            <h1>Atölye</h1>
           </div>
         </div>
 
